7906b3215fbfd2a9b2682fad502cffeb8767d00e
Kathrin Weny <weny@unfpa.local>
2018-02-21 17:22:50 -0500

    Push III February 21 2018

--- DHS.R
+++ DHS.R
@@ -9,7 +9,7 @@
 ###########
 
 # work is unfinished
-# standard error calculation takes a long time
+# standard error calculation takes a long time (more than 1 hour)
 
 ###########
 # file description comment, including purpose of program, inputs, and outputs
@@ -19,9 +19,8 @@
 # including non-sampling errors (confidence intervals)
 # the calculations are based on the microdataset of the 2016 DHS in Ethiopia
 
-
 ###########
-# source() and library() statements
+# source(), library() and options statements
 ###########
 
 library(plyr)
@@ -36,18 +35,17 @@ library(foreign)
 library(reshape2)
 library(survminer)
 
+options(scipen = 999)  # disable scientific notation in R
+
 ###########
 # function definitions
 ###########
-
-
 
 CalculateTimetoEvent <- function(x,y,z){
   # calculates the time (years) that has passed until a certain event(FGM) occurs
   # and in the case, there was no event, uses proxy (age)
   #
   #Args:
-  #   w = dataframe
   #   x = event indicator variable
   #   y = age at wich event occured
   #   z = substitute in case no event
@@ -61,55 +59,86 @@ CalculateTimetoEvent <- function(x,y,z){
   
   return(time)
 
-
 }
 
+ReadListofDTA <- function(x){
+  #Rreads in all DTA files in the directors and stores them in a named list
+  #
+  #Args:
+  # x = list of character values representing names of all DTA files in the directory
+  #Returns:
+  #  ldf     = list of all data frames in the directory named by x
+  
+  ldf <- list() # creates a list
+  
+  for (k in 1:length(x)){
+    ldf[[k]] <- read.dta(x[k], convert.factors=FALSE)
+  }
+  
+  names(ldf) <- x
+  
+  return(ldf)
+
+}
 
 ###########
 # executed statements
 ###########
 
-# read in data
+# set working directory
+setwd("C:/Users/weny/Google Drive/2018/FGM/01 -Survival Analysis/03 -Data/DHS")
 
-eth2016_wide <- read.dta("C:/Users/weny/Google Drive/2018/FGM/01 -Survival Analysis/01 -Ethiopia_DHS 2016/STATA/ETIR70FL.DTA", convert.factors=FALSE)
-# eth2016 <- read.dta("C:/Users/Kathrin Weny/Google Drive/FGM/Survival Analysis/Ethiopia_DHS 2016/STATA/ETIR70FL.dta", convert.factors=FALSE)
+# read in stata datafiles
+listdta <- dir(pattern = "*.DTA") # creates the list of all the csv files in the directory
 
-options(scipen = 999)  # disable scientific notation in R
+ldf <- ReadListofDTA(listdta) 
+
+# select relevant data file
+  # ethiopia
+#wide <- ldf[["ethiopia.DTA"]]
+
+# try a loop
+
+# create empty list to store results
+SmallSurvivalList <- list()
+PlotList <- list()
+
+for (i in length(ldf)){
+
+wide <- ldf[[i]]
 
 # select variables and create dataset for birth reshape and FGM reshape
-
-eth2016_wide_allchildren <- eth2016_wide %>%
+wide_allchildren <- wide %>%
   dplyr::select(c(1, 3:4, 7, 25:29, 51, 4864, 4866, 4867:4870, 4880, 66:67, 77, 87, 102, 3624, 227:246, 307:326))
 
-eth2016_wide_fgm <- eth2016_wide %>%
+wide_fgm <- wide %>%
   dplyr::select(c(1, 4894:4914, 4915:4994))
 
 # reshape FGM module
-
-eth2016_long_fgm <- reshape(eth2016_wide_fgm, varying = c(3:102), direction = "long", idvar = "caseid", sep = "_", timevar = "order")
+long_fgm <- reshape(wide_fgm, varying = c(3:102), direction = "long", idvar = "caseid", sep = "_", timevar = "order")
 
 # delete order
-eth2016_long_fgm <- eth2016_long_fgm %>%
+long_fgm <- long_fgm %>%
   dplyr::select(-order)
 
 # rename GIDX to order
-colnames(eth2016_long_fgm)[3] <- "order"
+colnames(long_fgm)[3] <- "order"
 
 # create ID for merge
-eth2016_long_fgm <- eth2016_long_fgm %>%
+long_fgm <- long_fgm %>%
   mutate(id.match = paste(caseid, order, sep = ""))
 
 # reshape ALL CHILDREN
 
-eth2016_long_allchildren <- reshape(eth2016_wide_allchildren, varying = c(24:63), direction = "long", idvar = "caseid", sep = "_", timevar="order")
+long_allchildren <- reshape(wide_allchildren, varying = c(24:63), direction = "long", idvar = "caseid", sep = "_", timevar="order")
 
 # create ID for merge
-eth2016_long_allchildren <- eth2016_long_allchildren %>%
+long_allchildren <- long_allchildren %>%
   mutate(id.match = paste(caseid, order, sep = ""))
 
 # merge data sets
 
-df <- merge(eth2016_long_fgm, eth2016_long_allchildren, by = "id.match")
+df <- merge(long_fgm, long_allchildren, by = "id.match")
 
 colnames(df)[2] <- "caseid"
 
@@ -127,6 +156,7 @@ df <- df %>%
 # create weight variable from v005
 df$wgt <- as.numeric(df$v005 / 1000000)
 
+# increase memory
 memory.limit(550000) 
 
 # create a small sample survey to calculate standard errors
@@ -137,8 +167,10 @@ SmallSurvey <- svydesign(id             = ~v021,
                          data           = df)
 
 SmallSurvival <- svykm(Surv(time, g121 > 0) ~ 1, design = SmallSurvey, se = TRUE)
+SmallSurvivalList[[i]]     <- SmallSurvival
 
 confint(SmallSurvival, parm = c(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14), level = 0.95)
 
-plot(SmallSurvival, pars = NULL, ci = TRUE)
-
+plot <- plot(SmallSurvival, pars = NULL, ci = TRUE)
+PlotList[[i]]     <- plot
+}
