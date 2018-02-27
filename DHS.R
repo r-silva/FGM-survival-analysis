0246940883146fdf79a6c971bae5f17cc40dbcb2
Kathrin Weny <weny@unfpa.local>
2018-02-27 18:06:36 -0500

    First commit 27 February

--- DHS.R
+++ DHS.R
@@ -1,49 +1,48 @@
-###########
-# copyright statement comment
-###########
+
+# Copyright statement comment ---------------------------------------------
 
 # @UNFPA
 
-###########
-# author comment
-###########
 
-# work is unfinished
-# standard error calculation takes a long time (more than 1 hour)
+# Author comment ----------------------------------------------------------
 
-###########
-# file description comment, including purpose of program, inputs, and outputs
-###########
+# work is progress-
+# Standard error calculation takes a long time (more than 1 hour).
+# For countries other than Tanzania, Cote d'Ivoire and Togo, complex sample SE have been calculated based on 
+# a sample of the actual data available. In addition, Se based on (weighted) random sampling have been added.
+# This proivdes an upper an a lower boundary for SEs
+
+
+# File description, purpose of code, inputs and output --------------------
 
 # this code has been written in order to estimate the survival curves for girls age 0-14 in Ethiopia
 # including non-sampling errors (confidence intervals)
 # the calculations are based on the microdataset of the 2016 DHS in Ethiopia
 
-###########
-# source(), library() and options statements
-###########
+
+# Source codes, libraries, global options and working directory -----------
 
 library(plyr)
 library(dplyr)
 library(magrittr)
 library(tidyverse)
-library(gridExtra)
-library(gridBase)
-library(readxl)
 library(survey)
 library(foreign)
 library(reshape2)
-library(survminer)
 
 options(scipen = 999)  # disable scientific notation in R
 
-setwd("C:/Users/weny/Google Drive/2018/FGM/01 -Survival Analysis/03 -Data/DHS")
+#if a primary sapling unit has only a single observation, R will crash
+# option adjust calculates conservative standard errors
+# reference: http://faculty.washington.edu/tlumley/survey/example-lonely.html
+options(survey.lonely.psu = "adjust")
 
-listdta <- dir(pattern = "*.DTA") # stores names of all DTA files in a list
+setwd("~/rstudio")
 
-###########
-# function definitions
-###########
+listdta <- dir(pattern = "*.DTA")
+
+
+# Function definitions ----------------------------------------------------
 
 CalculateTimetoEvent <- function(x,y,z){
   # calculates the time (years) that has passed until a certain event(FGM) occurs
@@ -67,7 +66,7 @@ CalculateTimetoEvent <- function(x,y,z){
 
 ReadListofDTA <- function(x){
   # reads in all DTA files in the directory and stores them in a named list
-  # uses a lot of memomry, in the case of the 11 DHS files 3 661 612 264 bytes
+  # uses a lot of memomry, in the case of the 11 DHS files 3 661 612 264 bytes, so better do not store in RAM
   # 
   # Args:
   # x = directory
@@ -102,124 +101,168 @@ ReadSingleDTA <- function(x){
 }
 
 
-###########
-# executed statements
-###########
+randomRows <- function(x,y){
+  # randomly selects y records from original filex
+  # 
+  # Args:
+  #  x = dataframe
+  #  y = number of lines to be selected
+  #
+  # Returns:
+  #  dataframe with y rows
+  #  reports error when more cases than available rows are selected
+  
+  if(y > nrow(x)) stop("too many cases select")
+  
+  return(x[sample(nrow(x),y),])
+  
+}
+
+
+# Executed statements -----------------------------------------------------
 
 # read in stata datafiles
 
-# for pcs with a lot of memory
+# for pcs with a lot of memory, loop in all DHS stata files and select one later
 # ldf <- ReadListofDTA() 
 
 # create empty list to store results in loop
 SmallSurvivalList <- list()
-PlotList <- list()
+ConInList         <- list()
+SmallSurvey_random<- list()
 
-#for (i in 1:length(listdta)){
-i<- 10
+# for (i in 1:length(listdta)){
+i<- 2
 
-    # for pcs with a lot of memory
-  #wide <- ldf[[i]]
-  
-  # if limited memory
-  wide <- ReadSingleDTA(i) 
-  
-  # select variables and create dataset for birth reshape and FGM reshape
-  wide_allchildren <- wide %>%
-    dplyr::select(c("caseid","v001","v002","v005","v021","v022","v023","v024","v025",
-                    "bidx_01","bidx_02","bidx_03","bidx_04","bidx_05","bidx_06","bidx_07","bidx_08","bidx_09","bidx_10","bidx_11","bidx_12","bidx_13","bidx_14","bidx_15","bidx_16","bidx_17","bidx_18","bidx_19","bidx_20",
-                    "b4_01","b4_02","b4_03","b4_04","b4_05","b4_06","b4_07","b4_08","b4_09","b4_10","b4_11","b4_12","b4_13","b4_14","b4_15","b4_16","b4_17","b4_18","b4_19","b4_20",
-                    "b8_01","b8_02","b8_03","b8_04","b8_05","b8_06","b8_07","b8_08","b8_09","b8_10","b8_11","b8_12","b8_13","b8_14","b8_15","b8_16","b8_17","b8_18","b8_19","b8_20"))
-  
-  wide_fgm <- wide %>%
-    dplyr::select(c("caseid","v001",
-                    "gidx_01","gidx_02","gidx_03","gidx_04","gidx_05","gidx_06","gidx_07","gidx_08","gidx_09","gidx_10","gidx_11","gidx_12","gidx_13","gidx_14","gidx_15","gidx_16","gidx_17","gidx_18","gidx_19","gidx_20",
-                    "g121_01","g121_02","g121_03","g121_04","g121_05","g121_06","g121_07","g121_08","g121_09","g121_10","g121_11","g121_12","g121_13","g121_14","g121_15","g121_16","g121_17","g121_18","g121_19","g121_20",
-                    "g122_01","g122_02","g122_03","g122_04","g122_05","g122_06","g122_07","g122_08","g122_09","g122_10","g122_11","g122_12","g122_13","g122_14","g122_15","g122_16","g122_17","g122_18","g122_19","g122_20",
-                    "g123_01","g123_02","g123_03","g123_04","g123_05","g123_06","g123_07","g123_08","g123_09","g123_10","g123_11","g123_12","g123_13","g123_14","g123_15","g123_16","g123_17","g123_18","g123_19","g123_20",
-                    "g124_01","g124_02","g124_03","g124_04","g124_05","g124_06","g124_07","g124_08","g124_09","g124_10","g124_11","g124_12","g124_13","g124_14","g124_15","g124_16","g124_17","g124_18","g124_19","g124_20"))
-  
-  
-  # reshape FGM module
-  long_fgm <- reshape(wide_fgm, varying = c(3:102), direction = "long", idvar = "caseid", sep = "_", timevar = "order")
-  
-  # delete order
-  long_fgm <- long_fgm %>%
-    dplyr::select(-order)
-  
-  # rename GIDX to order
-  colnames(long_fgm)[3] <- "order"
-  
-  # create ID for merge
-  long_fgm <- long_fgm %>%
-    mutate(id.match = paste(caseid, order, sep = ""))
-  
-  # reshape ALL CHILDREN
-  
-  long_allchildren <- reshape(wide_allchildren, varying = c(10:69), direction = "long", idvar = "caseid", sep = "_", timevar="order")
-  
-  # create ID for merge
-  long_allchildren <- long_allchildren %>%
-    mutate(id.match = paste(caseid, order, sep = ""))
-  
-  # merge data sets
-  
-  df <- merge(long_fgm, long_allchildren, by = "id.match")
-  
-  colnames(df)[2] <- "caseid"
-  
-  # add time to event column
-  
-  time <- CalculateTimetoEvent(df$g121,df$g122,df$b8)
-  
-  df <- df %>%
-    mutate(time = time)
-  
-  # add id column
-  df <- df %>%
-    mutate(id = 1)
-  
-  #NAs
-  df$time <- ifelse(df$time==98 | df$time==99,NA,df$time)
-  
-  # create weight variable from v005
-  df$wgt <- as.numeric(df$v005 / 1000000)
-  
-  # increase memory
-  memory.limit(10000000) 
-  
-  # create a small sample survey to calculate standard errors
-  SmallSurvey <- svydesign(id             = ~v021, 
-                           strata         = df$v022, 
-                           variables      = ~g121 + time,
-                           weight         = df$wgt,
-                           data           = df)
-  
-  # free memomry for KM estimates
-  rm(wide, wide_allchildren,long_allchildren, wide_fgm,long_fgm,df,time)
+# for pcs with a lot of memory select one DHS stat file from list ldf created above
+# wide <- ldf[[i]]
 
-  # KM estimate
-  SmallSurvival <- svykm(Surv(time, g121 > 0) ~ 1, design = SmallSurvey, se = TRUE)
-  SmallSurvivalList[[i]] <- SmallSurvival  # store survival object in list
-  
-  confint(SmallSurvival, parm = c(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14), level = 0.95)
-  
-  # plot
-  plot <- plot(SmallSurvival, pars = NULL, ci = TRUE)
-  PlotList[[i]] <- plot  # store plot in list
+# if limited memory only read in one DHS stat file at a time
+wide <- ReadSingleDTA(i) 
+
+# wide <- read.dta("burkina_faso.DTA", convert.factors = FALSE)
+
+# select variables and create dataset for birth reshape and FGM reshape
+wide_allchildren <- wide %>%
+  dplyr::select(c("caseid","v001","v002","v005","v021","v022","v023","v024","v025",
+                  "bidx_01","bidx_02","bidx_03","bidx_04","bidx_05","bidx_06","bidx_07","bidx_08","bidx_09","bidx_10","bidx_11","bidx_12","bidx_13","bidx_14","bidx_15","bidx_16","bidx_17","bidx_18","bidx_19","bidx_20",
+                  "b4_01","b4_02","b4_03","b4_04","b4_05","b4_06","b4_07","b4_08","b4_09","b4_10","b4_11","b4_12","b4_13","b4_14","b4_15","b4_16","b4_17","b4_18","b4_19","b4_20",
+                  "b8_01","b8_02","b8_03","b8_04","b8_05","b8_06","b8_07","b8_08","b8_09","b8_10","b8_11","b8_12","b8_13","b8_14","b8_15","b8_16","b8_17","b8_18","b8_19","b8_20"))
+
+wide_fgm <- wide %>%
+  dplyr::select(c("caseid","v001",
+                  "gidx_01","gidx_02","gidx_03","gidx_04","gidx_05","gidx_06","gidx_07","gidx_08","gidx_09","gidx_10","gidx_11","gidx_12","gidx_13","gidx_14","gidx_15","gidx_16","gidx_17","gidx_18","gidx_19","gidx_20",
+                  "g121_01","g121_02","g121_03","g121_04","g121_05","g121_06","g121_07","g121_08","g121_09","g121_10","g121_11","g121_12","g121_13","g121_14","g121_15","g121_16","g121_17","g121_18","g121_19","g121_20",
+                  "g122_01","g122_02","g122_03","g122_04","g122_05","g122_06","g122_07","g122_08","g122_09","g122_10","g122_11","g122_12","g122_13","g122_14","g122_15","g122_16","g122_17","g122_18","g122_19","g122_20",
+                  "g123_01","g123_02","g123_03","g123_04","g123_05","g123_06","g123_07","g123_08","g123_09","g123_10","g123_11","g123_12","g123_13","g123_14","g123_15","g123_16","g123_17","g123_18","g123_19","g123_20",
+                  "g124_01","g124_02","g124_03","g124_04","g124_05","g124_06","g124_07","g124_08","g124_09","g124_10","g124_11","g124_12","g124_13","g124_14","g124_15","g124_16","g124_17","g124_18","g124_19","g124_20"))
+
+
+# reshape FGM module
+long_fgm <- reshape(wide_fgm, varying = c(3:102), direction = "long", idvar = "caseid", sep = "_", timevar = "order")
+
+# delete order
+long_fgm <- long_fgm %>%
+  dplyr::select(-order)
+
+# rename GIDX to order
+colnames(long_fgm)[3] <- "order"
+
+# create ID for merge
+long_fgm <- long_fgm %>%
+  mutate(id.match = paste(caseid, order, sep = ""))
+
+# reshape ALL CHILDREN
+
+long_allchildren <- reshape(wide_allchildren, varying = c(10:69), direction = "long", idvar = "caseid", sep = "_", timevar="order")
+
+# create ID for merge
+long_allchildren <- long_allchildren %>%
+  mutate(id.match = paste(caseid, order, sep = ""))
+
+# merge data sets
+
+df <- merge(long_fgm, long_allchildren, by = "id.match")
+
+colnames(df)[2] <- "caseid"
+
+# add time to event column
+
+time <- CalculateTimetoEvent(df$g121, df$g122, df$b8)
+
+df <- df %>%
+  mutate(time = time)
+
+# add id column that is 1 for each recrod in order to count unweighted and weighted population sizes
+df <- df %>%
+  mutate(id = 1)
+
+#NAs
+df$time <- ifelse(df$time == 98 | df$time == 99, NA, df$time)
+
+# create weight variable from v005
+df$wgt <- as.numeric(df$v005 / 1000000)
+
+# Reduce df to only absolutely necessary variables and records
+df <- df %>%
+  select(v021,v022,g121,time,wgt)
+
+# increase memory, is Windows specific, R server runs in Ubuntu
+# memory.limit(10000000) 
+
+dim(df)
+
+ifelse(i == 1 | i == 2 | i == 4 |
+         i == 5 | i == 6 | i == 7 |
+         i == 8, df <- randomRows(df,2500),df)
+
+dim(df)
+
+
+# create a small sample survey to calculate standard errors with taylor-series linearization
+SmallSurvey <- svydesign(id             = ~v021, 
+                         strata         = df$v022, 
+                         # variables      = ~g121 + time,
+                         weight         = df$wgt,
+                         data           = df)
+
+# free RAM for KM estimates, remove previous dataframes and objects
+rm(wide, wide_allchildren,long_allchildren, wide_fgm,long_fgm,time)
+
+# garbage colleaction: clear up RAM
+gc()
+
+# KM estimate
+SmallSurvival <- svykm(Surv(time, g121 > 0) ~ 1, design = SmallSurvey, se = TRUE)
+SmallSurvivalList[[i]] <- SmallSurvival  # store survival object in list
+plot(SmallSurvival)
+
+# Store standard errors
+ses <- confint(SmallSurvival, parm = c(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14), level = 0.95)
+ConInList[[i]] <- ses  
+
 #}
 
-  library (data.table)
-str(SmallSurvivalList[[10]]$varlog)
 
-x <- as.data.table(SmallSurvivalList[[10]]$varlog)
+# Standard error based on random sampling ---------------------------------
 
-plot(SmallSurvival, pars = NULL, ci = TRUE, ylim=c(0.985,1.0))
+SmallSurvey_randomdesign <- survfit(Surv(time, g121) ~ 1 , data=df,
+                                    weight=wgt) # status 9 is converted to NA
+
+plot(SmallSurvey_randomdesign)
+
+SmallSurvey_random_conf <- setNames(data.frame(matrix(ncol = 2, nrow = 17)), c("lower_random","upper_random"))
+SmallSurvey_random_conf[,1] <- as.data.frame(SmallSurvey_randomdesign$lower)
+SmallSurvey_random_conf[,2] <- as.data.frame(SmallSurvey_randomdesign$upper)
+
+SmallSurvey_random[[i]] <- SmallSurvey_random_conf
 
 
+# Results -----------------------------------------------------------------
 
-
-
+plot(SmallSurvivalList[[i]])
+ConInList[[i]]
+SmallSurvey_random[[i]]
 
 
 
